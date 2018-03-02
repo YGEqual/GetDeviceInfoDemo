66a3d069ae9eaa490d6cad0d914bff031e110b8f
YGEqual <ft@ftdeMacBook-Pro.local>
2018-03-02 11:25:54 +0800

    Initial Commit

---
+++ GetDeviceInfoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetDeviceInfoDemo
+//
+//  Created by ft on 2018/3/2.
+//  Copyright © 2018年 ft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
